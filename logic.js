6463e06fe2219c2692f815f93f454d5002ca4b8c
AyushVH <99384683+AyushVH@users.noreply.github.com>
2022-05-20 23:02:30 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -6,21 +6,17 @@ let p_pt = 0;
 let c_pt = 0;
 let comp_pt = document.getElementById("cpt");
 let player_pt = document.getElementById("ppt");
+let result = document.getElementsByClassName("result")[0];
+let comp_img = document.getElementById("comp-img");
+let player_img = document.getElementById("player-img");
 
 
-function gamePlay(b) {
+function gamePlay(e) {
     let randNum = getRndInteger(1, 3);
-    let btn_img = b.getAttribute("src");
-
-    let result = document.getElementsByClassName("result")[0];
-    let comp_img = document.getElementById("comp-img");
-    let player_img = document.getElementById("player-img");
-
-
+    let btn_img = e.target.getAttribute("src");
 
     if ((btn_img == "images/stoneR.png") && (randNum == 1)) {
         comp_img.src = "images/stoneL.png";
-        player_img.src = "images/stoneR.png";
         result.innerText = "TIE"
         result.style = "color: yellow;"
 
@@ -28,19 +24,15 @@ function gamePlay(b) {
 
     else if ((btn_img == "images/stoneR.png") && (randNum == 2)) {
         comp_img.src = "images/paperL.png";
-        player_img.scr = "images/stoneR.png";
         result.innerText = "You Lose"
         result.style = "color: red;"
         c_pt += 1;
         comp_pt.innerText = c_pt;
 
-
-
     }
 
     else if ((btn_img == "images/stoneR.png") && (randNum == 3)) {
         comp_img.src = "images/scissorL.png";
-        player_img.scr = "images/stoneR.png";
         result.innerText = "You Win";
         result.style = "color: rgb(107, 235, 52);"
         p_pt += 1;
@@ -50,7 +42,6 @@ function gamePlay(b) {
 
     else if ((btn_img == "images/paperR.png") && (randNum == 1)) {
         comp_img.src = "images/stoneL.png";
-        player_img.src = "images/paperR.png";
         result.innerText = "You Win";
         result.style = "color: rgb(107, 235, 52);"
         p_pt += 1;
@@ -60,7 +51,6 @@ function gamePlay(b) {
 
     else if ((btn_img == "images/paperR.png") && (randNum == 2)) {
         comp_img.src = "images/paperL.png";
-        player_img.src = "images/paperR.png";
         result.innerText = "TIE"
         result.style = "color: yellow;"
 
@@ -68,7 +58,6 @@ function gamePlay(b) {
 
     else if ((btn_img == "images/paperR.png") && (randNum == 3)) {
         comp_img.src = "images/scissorL.png";
-        player_img.src = "images/paperR.png";
         result.innerText = "You Lose"
         result.style = "color: red;"
         c_pt += 1;
@@ -78,7 +67,6 @@ function gamePlay(b) {
 
     else if ((btn_img == "images/scissorR.png") && (randNum == 1)) {
         comp_img.src = "images/stoneL.png";
-        player_img.src = "images/scissorR.png"
         result.innerText = "You Lose"
         result.style = "color: red;"
         c_pt += 1;
@@ -88,7 +76,6 @@ function gamePlay(b) {
 
     else if ((btn_img == "images/scissorR.png") && (randNum == 2)) {
         comp_img.src = "images/paperL.png";
-        player_img.src = "images/scissorR.png"
         result.innerText = "You Win";
         result.style = "color: rgb(107, 235, 52);"
         p_pt += 1;
@@ -98,9 +85,8 @@ function gamePlay(b) {
 
     else if ((btn_img == "images/scissorR.png") && (randNum == 3)) {
         comp_img.src = "images/scissorL.png";
-        player_img.src = "images/scissorR.png"
-        result.innerText = "TIE"
-        result.style = "color: yellow;"
+        result.innerText = "TIE";
+        result.style = "color: yellow;";
 
     }
 
@@ -126,10 +112,20 @@ function point_color() {
 }
 
 let stone = document.getElementById("stn");
-stone.addEventListener("click", (function () { gamePlay(stone) }));
+stone.addEventListener("click", () => {
+    player_img.src = "images/stoneR.png";
+});
+stone.addEventListener("click", gamePlay);
+
 
 let paper = document.getElementById("ppr");
-paper.addEventListener("click", (function () { gamePlay(paper) }));
+paper.addEventListener("click", () => {
+    player_img.src = "images/paperR.png";;
+});
+paper.addEventListener("click", gamePlay);
 
 let scissor = document.getElementById("scr");
-scissor.addEventListener("click", (function () { gamePlay(scissor) }));
+scissor.addEventListener("click", () => {
+    player_img.src = "images/scissorR.png";
+});
+scissor.addEventListener("click", gamePlay);
